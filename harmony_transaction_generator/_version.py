ed177fdcfeda635d7fa060c661e5947746a3c7cc
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2020-02-04 00:42:06 -0800

    update version

--- harmony_transaction_generator/_version.py
+++ harmony_transaction_generator/_version.py
@@ -7,5 +7,5 @@ Provides harmony_transaction_generator version information.
 
 from incremental import Version
 
-__version__ = Version('harmony_transaction_generator', 20, 1, 32)
+__version__ = Version('harmony_transaction_generator', 20, 1, 33)
 __all__ = ["__version__"]
